6fe02b9eaba68f20e4cd4e52112331d4d81c5cd0
gegeqi <ccczzz1015@163.com>
2016-05-28 23:01:40 +0800

    Update separators.js

--- thousands_separators/src/separators.js
+++ thousands_separators/src/separators.js
@@ -1,20 +1,11 @@
 'use strict';
 
-<<<<<<< HEAD
-function thousands_separators(num) {
-    var expectText = String(input).split(".");
-    expectText[0] = expectText[0].replace(new RegExp('(\\d)(?=(\\d{3})+$)','ig'),"$1,");
-    return expectText.join(".");
-  
-=======
 function thousands_separators(input) {
     var expectText = String(input).split(".");
     expectText[0] = expectText[0].replace(new RegExp('(\\d)(?=(\\d{3})+$)','ig'),"$1,");
     return expectText.join(".");
 
->>>>>>> 4bf256a026d016d3d523e5d9a22feded403ee9ba
 }
-
 
 module.exports = thousands_separators;
 
